06ffa9ee5506e06e09c62486718327172199fb18
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-06 17:07:05 +0100

    getting started - getting pandas to read csv and checking if current date is equal to someones birthday

--- main.py
+++ main.py
@@ -9,5 +9,22 @@
 # 4. Send the letter generated in step 3 to that person's email address.
 
 
+import datetime as dt
+import smtplib
+from random import  choice
+import pandas
+
+curr_date = dt.datetime.now()
+
+data = pandas.read_csv("birthdays.csv")
+all_contacts_details = data.to_dict(orient="records")
+
+today = dt.datetime(year=curr_date.year, month=curr_date.month, day=curr_date.day)
+
+for person in all_contacts_details:
+    if person["month"] == today.month and person["day"] == today.day:
+        print(f"ITS YOUR BIRTHDAY {person['name']}")
+    else:
+        print(f"ITS NOT YOUR BIRTHDAY {person['name']}")
 
 
